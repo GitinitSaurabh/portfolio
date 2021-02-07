5e63b84d9ac422fc3a875eee782d6bb6382b19e3
GitinitSaurabh <saurabh160196@gmail.com>
2021-02-07 21:40:38 +0530

    Added date field to Blog Model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=50)
-    body = models.CharField(max_length=1000)
-    image = models.ImageField(upload_to='blog/images/')
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='blog/images/', blank=True)
+    date = models.DateField()
