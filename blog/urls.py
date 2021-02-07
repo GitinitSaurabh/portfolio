9545c10e7cb65cbfde38cd4e75cf235567b967a6
GitinitSaurabh <saurabh160196@gmail.com>
2021-02-08 00:59:31 +0530

    Added blogs/blog details page/bootstrap/icons

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
-    
+    path('<int:blog_id>', views.detail, name='detail'),
+
+
 ]
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -21,7 +21,7 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
 
 ]
